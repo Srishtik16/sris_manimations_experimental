7f52d9ab1fc8b8f194d71417612e8e9efe27d3f2
Srishtik Dutta <srishtikdutta@Srishtiks-MacBook-Air.local>
2025-03-15 21:03:47 +0530

    Added array class animation by passing array

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
 from manim import *
+from Array import CreateArray
 
 
-class CreateCircle(Scene):
-    def construct(self):
-        rectangle = Rectangle(width=5, height=1, color=WHITE)
-        self.add(rectangle)
-        self.wait(3)
-        lines = VGroup(
-            *[Line(start=[x, -0.5, 0], end=[x, 0.5, 0], color=WHITE)
-              for x in [-1.5, -0.5, 0.5, 1.5]]  # Positions of the lines
-        )
-        self.play(Create(lines))
-        self.wait(2)
+class MainScene(CreateArray):
+    def __init__(self, **kwargs):
+        super().__init__([1, 2, 3, 4, 5, 6], **kwargs)  # Define an array
 
-        numbers = VGroup(
-            *[
-                MathTex(r"\mathbf{" + str(i) + "}").move_to([x, 0, 0])
-                for i, x in enumerate([-2, -1, 0, 1, 2], start=1)
-            ]
-        )
-
-        self.play(Create(numbers))
-        self.wait(3)
-
-        redNumbers = VGroup(
-            *[
-                MathTex(r"\mathbf{" + str(x[0]) + "}", color=RED).move_to([x[1], 0, 0])
-                for i, x in enumerate([[2, -1], [5, 2]], start=1)
-            ]
-        )
-
-        self.play(Create(redNumbers))
-        self.wait(3)
-
-        self.play(FadeOut(numbers[1]), FadeOut(numbers[4]))
+if __name__ == "__main__":
+    scene = MainScene()
+    scene.render()
 
 
-        self.play(FadeOut(redNumbers))
-        self.wait(1)
+# class CreateCircle(Scene):
+#     def construct(self):
+#         rectangle = Rectangle(width=5, height=1, color=WHITE)
+#         self.add(rectangle)
+#         self.wait(3)
+#         lines = VGroup(
+#             *[Line(start=[x, -0.5, 0], end=[x, 0.5, 0], color=WHITE)
+#               for x in [-1.5, -0.5, 0.5, 1.5]]  # Positions of the lines
+#         )
+#         self.play(Create(lines))
+#         self.wait(2)
 
-        transformedRedNumbers = VGroup(
-            *[
-                MathTex(r"\mathbf{" + str(x[0]) + "}", color=RED).move_to([x[1], 0, 0])
-                for i, x in enumerate(([5, -1], [2, 2]), start=1)
-            ]
-        )
+#         numbers = VGroup(
+#             *[
+#                 MathTex(r"\mathbf{" + str(i) + "}").move_to([x, 0, 0])
+#                 for i, x in enumerate([-2, -1, 0, 1, 2], start=1)
+#             ]
+#         )
 
-        self.play(Create(transformedRedNumbers))
-        self.wait(3)
+#         self.play(Create(numbers))
+#         self.wait(3)
+
+#         redNumbers = VGroup(
+#             *[
+#                 MathTex(r"\mathbf{" + str(x[0]) + "}", color=RED).move_to([x[1], 0, 0])
+#                 for i, x in enumerate([[2, -1], [5, 2]], start=1)
+#             ]
+#         )
+
+#         self.play(Create(redNumbers))
+#         self.wait(3)
+
+#         self.play(FadeOut(numbers[1]), FadeOut(numbers[4]))
+
+
+#         self.play(FadeOut(redNumbers))
+#         self.wait(1)
+
+#         transformedRedNumbers = VGroup(
+#             *[
+#                 MathTex(r"\mathbf{" + str(x[0]) + "}", color=RED).move_to([x[1], 0, 0])
+#                 for i, x in enumerate(([5, -1], [2, 2]), start=1)
+#             ]
+#         )
+
+#         self.play(Create(transformedRedNumbers))
+#         self.wait(3)
